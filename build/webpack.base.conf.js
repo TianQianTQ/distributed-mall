f13164fe3d1801c775d0ee92fbabe68e0deaf9ad
田倩 <tianqian01@finupgroup.com>
2018-04-28 19:53:23 +0800

    init

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,17 +1,13 @@
-'use strict'
-const path = require('path')
-const utils = require('./utils')
-const config = require('../config')
-const vueLoaderConfig = require('./vue-loader.conf')
+var path = require('path')
+var utils = require('./utils')
+var config = require('../config')
+var vueLoaderConfig = require('./vue-loader.conf')
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
 }
 
-
-
 module.exports = {
-  context: path.resolve(__dirname, '../'),
   entry: {
     app: './src/main.js'
   },
@@ -25,21 +21,38 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.vue', '.json'],
     alias: {
-      'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
+      '/components': resolve('src/components'),
+      '/page': resolve('src/page'),
+      '/common': resolve('src/common'),
+      '/assets': resolve('src/assets'),
+      '/api': resolve('src/api'),
+      '/utils': resolve('src/utils')
     }
   },
   module: {
     rules: [
       {
+        test: /\.(js|vue)$/,
+        loader: 'eslint-loader',
+        enforce: 'pre',
+        include: [resolve('src'), resolve('test')],
+        options: {
+          formatter: require('eslint-friendly-formatter')
+        }
+      },
+      {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: vueLoaderConfig
+      }, {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass']
       },
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
+        include: [resolve('src'), resolve('test')]
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
@@ -47,14 +60,6 @@ module.exports = {
         options: {
           limit: 10000,
           name: utils.assetsPath('img/[name].[hash:7].[ext]')
-        }
-      },
-      {
-        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: utils.assetsPath('media/[name].[hash:7].[ext]')
         }
       },
       {
@@ -66,17 +71,5 @@ module.exports = {
         }
       }
     ]
-  },
-  node: {
-    // prevent webpack from injecting useless setImmediate polyfill because Vue
-    // source contains it (although only uses it if it's native).
-    setImmediate: false,
-    // prevent webpack from injecting mocks to Node native modules
-    // that does not make sense for the client
-    dgram: 'empty',
-    fs: 'empty',
-    net: 'empty',
-    tls: 'empty',
-    child_process: 'empty'
   }
 }
--- src/App.vue
+++ src/App.vue
@@ -1,91 +1,21 @@
 <template>
   <div id="app">
-    <h1 v-text="title"></h1>
-    <input v-model="newItem"
-           @keyup.enter="addNew"
-    >
-    <ul>
-      <li v-for="item in items"
-          v-bind:class="{finished:item.isFinished}"
-          @click="click(item)"
-      >{{item.label}}</li>
-    </ul>
-    <component-a messageFromfather="i am your father"
-    v-on:children-tellmesomething="listenmyboy"></component-a>
-    <p>children tells me:{{childwords}}</p>
+    <router-view class="main"></router-view>
   </div>
 </template>
-
 <script>
-  import Store from './store'
-  import componentA from './components/componentA'
-  console.log(Store);
-export default {
-  components: {componentA},
-  data:function(){
-    return {
-      title:'this is a todo list',
-      items:Store.fetch(),
-      newItem:'',
-      childwords:'',
-    }
-  },
-  watch:{
-    items:{
-      handler:function(items){
-          Store.save(items);
-      },
-      deep:true
-    }
-  },
-  methods:{
-    listenmyboy:function(msg){
-       this.childwords = msg;
-    },
-    click:function(item){
-      console.log('clcik'+item.label);
-      item.isFinished = !item.isFinished;
-    },
-    addNew:function(){
-      this.items.push({
-        label:this.newItem,
-        isFinished:false
-      })
-        this.newItem = ''
-    }
+  export default {
+    name: 'app'
   }
-}
 </script>
+<style lang="scss" rel="stylesheet/scss">
+  @import "assets/style/index.scss";
 
-<style>
-  .finished{
-    text-decoration:underline;
-  }
-html{
-  height:100%;
-}
-body{
-  display:flex;
-  align-items:center;
-  justify-content: center;
-  height:100%;
-}
   #app {
-    color: #2c3e50;
-    margin-top: -100px;
-    max-width: 600px;
-    font-family: "Source Code Pro", Helvetica, SansSerif;
-    text-align: center;
+    height: 100%;
   }
-  #app a{
-    color:#42b983;
-    text-decoration:none;
-  }
-  .logo{
-    width:100px;
-    height:100px;
-  }
-  ul li{
-    cursor:pointer;
+
+  .main {
+    background: #ededed;;
   }
 </style>
